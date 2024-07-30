29070c378fe6627f0a305ee5f8bf7e9479a586ab
Solita43 <meli.matrix@outlook.com>
2024-07-29 23:01:13 -0700

    Add scheduled task to refresh top players data

--- server/config/celery.py
+++ server/config/celery.py
@@ -1,5 +1,7 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
+
 
 # Set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
@@ -14,3 +16,10 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django apps.
 app.autodiscover_tasks()
+
+app.conf.beat_schedule = {
+    'refresh-every-24-hours': {
+        'task': 'open_dota.tasks.refresh_top_players_data',
+        'schedule': crontab(minute='*/5', hour="*"),  # Execute daily at midnight
+    },
+}
